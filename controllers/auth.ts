359251ea2cd9af98e31ff52ab19115e372cfa6b7
Jose Daniel Martinez M <mmjosedaniel@hotmail.com>
2023-06-15 16:34:01 -0500

    Add: validation to signup

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,10 +1,19 @@
 import { RequestHandler } from 'express'
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken';
+import { validationResult } from 'express-validator';
 
 import User from '../models/user'
 
 export const signup: RequestHandler = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error: any = new Error('Validation failed!');
+		error.statusCode = 422;
+		error.data = errors.array();
+		throw error;
+	}
+
 	const email = req.body.email;
 	const name = req.body.name;
 	const password = req.body.password;
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,11 +1,34 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+
+import User from '../models/user';
+
 import { login, signup } from '../controllers/auth';
 
 
 
 const router = Router();
 
-router.put('/signup', signup);
+router.put(
+	'/signup',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Please enter a valid email.')
+			.custom((value, { req }) => {
+				return User.findOne({ email: value })
+				.then(userDoc => {
+					if (userDoc) {
+						return Promise.reject('Email adress already exists!');
+					};
+				});
+			})
+			.normalizeEmail(),
+		body('password').trim().isLength({ min: 4 }),
+		body('name').trim().not().isEmpty()
+	],
+	signup
+);
 
 router.post('/login', login);
 
